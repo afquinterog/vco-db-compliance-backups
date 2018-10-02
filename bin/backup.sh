1944c1cbbf8fac8631447a96f96b7a12d1cedbc1
Andres Quintero <aquintero@oru.com.co>
2018-10-02 17:41:04 -0500

    Save database backups on glacier

--- bin/backup.sh
+++ bin/backup.sh
@@ -13,10 +13,30 @@ if [[ -z "$DATABASE" ]]; then
   exit 1
 fi
 
-if [[ -z "$S3_BUCKET_PATH" ]]; then
-  echo "Missing S3_BUCKET_PATH variable which must be set the directory in s3 where you would like to store your database backups"
+if [[ -z "$VCO_BACKUPS_ACCESS_KEY_ID" ]]; then
+  echo "Missing VCO_BACKUPS_ACCESS_KEY_ID variable which must be set to the AWS access key"
   exit 1
 fi
+
+if [[ -z "$VCO_BACKUPS_SECRET_ACCESS_KEY" ]]; then
+  echo "Missing VCO_BACKUPS_SECRET_ACCESS_KEY variable which must be set to the AWS secret access key"
+  exit 1
+fi
+
+if [[ -z "$VCO_BACKUPS_REGION" ]]; then
+  echo "Missing VCO_BACKUPS_REGION variable which must be set to the AWS region"
+  exit 1
+fi
+
+
+
+
+#Get environment variables
+#VCO_BACKUPS_ACCESS_KEY_ID=$(cat $ENV_DIR/VCO_BACKUPS_ACCESS_KEY_ID)
+#VCO_BACKUPS_SECRET_ACCESS_KEY=$(cat $ENV_DIR/VCO_BACKUPS_SECRET_ACCESS_KEY)
+#VCO_BACKUPS_REGION=$(cat $ENV_DIR/VCO_BACKUPS_REGION)
+#VCO_BACKUPS_VAULT_NAME=$(cat $ENV_DIR/VCO_BACKUPS_VAULT_NAME)
+#VCO_BACKUPS_APP_NAME=$(cat $ENV_DIR/VCO_BACKUPS_APP_NAME)
 
 #install aws-cli
 curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
@@ -24,10 +44,19 @@ unzip awscli-bundle.zip
 chmod +x ./awscli-bundle/install
 ./awscli-bundle/install -i /tmp/aws
 
+#set backup name
 BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
 
+#configure aws cli
+/tmp/aws/bin/aws configure set aws_access_key_id $VCO_BACKUPS_ACCESS_KEY_ID
+/tmp/aws/bin/aws configure set aws_secret_access_key $VCO_BACKUPS_SECRET_ACCESS_KEY
+/tmp/aws/bin/aws configure set region $VCO_BACKUPS_REGION
+
+
+echo "-----> Generating backup ... "
 heroku pg:backups capture $DATABASE --app $APP
 curl -o $BACKUP_FILE_NAME `heroku pg:backups:url --app $APP`
+
 FINAL_FILE_NAME=$BACKUP_FILE_NAME
 
 if [[ -z "$NOGZIP" ]]; then
@@ -35,10 +64,16 @@ if [[ -z "$NOGZIP" ]]; then
   FINAL_FILE_NAME=$BACKUP_FILE_NAME.gz
 fi
 
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
 
-/tmp/aws/bin/aws s3 cp $FINAL_FILE_NAME s3://$S3_BUCKET_PATH/$APP/$DATABASE/$FINAL_FILE_NAME --sse AES256
+echo "-----> backing up to glacier ... "
 
-echo "backup $FINAL_FILE_NAME complete"
+#/tmp/aws/bin/aws s3 cp $FINAL_FILE_NAME s3://$S3_BUCKET_PATH/$APP/$DATABASE/$FINAL_FILE_NAME --sse AES256
+
+
+#Save data on Glacier vault
+echo "-----> Archive description: $FINAL_FILE_NAME" 
+/tmp/aws/bin/aws glacier upload-archive --archive-description $FINAL_FILE_NAME \
+  --account-id -  \
+  --vault-name $VCO_BACKUPS_VAULT_NAME \
+  --body FINAL_FILE_NAME 
 
