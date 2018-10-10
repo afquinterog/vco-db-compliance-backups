9e0de22f97b647099adf20e14dc96758548fd794
Andres Quintero <aquintero@oru.com.co>
2018-10-10 16:11:03 -0500

    Change message format

--- bin/backup.sh
+++ bin/backup.sh
@@ -46,30 +46,29 @@ fi
 	/tmp/aws/bin/aws configure set region $VCO_BACKUPS_REGION
 
 
-	#echo "-----> Generating backup ... "
-	#heroku pg:backups capture $DATABASE --app $APP
-	#curl -o $BACKUP_FILE_NAME `heroku pg:backups:url --app $APP`
-	#FINAL_FILE_NAME=$BACKUP_FILE_NAME
+	echo "-----> Generating backup ... "
+	heroku pg:backups capture $DATABASE --app $APP
+	curl -o $BACKUP_FILE_NAME `heroku pg:backups:url --app $APP`
+	FINAL_FILE_NAME=$BACKUP_FILE_NAME
 
 
-	#echo "-----> Compress backup .... "
-	#tar -czPf /tmp/backup.tgz $FINAL_FILE_NAME > /dev/null
+	echo "-----> Compress backup .... "
+	tar -czPf /tmp/backup.tgz $FINAL_FILE_NAME > /dev/null
 
-	#echo "-----> backing up to glacier .... "
+	echo "-----> backing up to glacier .... "
 
 	#Save data on Glacier vault
-	#echo "-----> Archive description: $FINAL_FILE_NAME" 
-	#/tmp/aws/bin/aws glacier upload-archive --archive-description $FINAL_FILE_NAME \
-	#  --account-id -  \
-	#  --vault-name $VCO_BACKUPS_VAULT_NAME \
-	#  --body /tmp/backup.tgz 
+	echo "-----> Archive description: $FINAL_FILE_NAME" 
+	/tmp/aws/bin/aws glacier upload-archive --archive-description $FINAL_FILE_NAME \
+	  --account-id -  \
+	  --vault-name $VCO_BACKUPS_VAULT_NAME \
+	  --body /tmp/backup.tgz 
 
 	#notify the sns topic
 	/tmp/aws/bin/aws sns publish --topic-arn "arn:aws:sns:us-east-1:847662935904:vco-production-backups-topic" \
-	 	--message "A new database backup has been generated for the $APP and has been sent to glacier from heroku."
-
+	 	--message "A new database backup has been generated for the $APP and has been sent to glacier."
 #else
-#	echo "-----> Backups will run once a week .... "	
+	#echo "-----> Backups will run once a week .... "	
 #fi
 
 
